69a6802e63ef1fd5749091a37d6e1796bda4bcd5
aniki219 <aniki219@gmail.com>
2021-10-14 11:43:36 -0700

    pre virginia

---
+++ Assets/PokeballParticleController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokeballParticleController : MonoBehaviour
+{
+    public enum PokeballType {
+        CHERISH,
+        DIVE,
+        DREAM,
+        DUSK,
+        GREAT,
+        HEAL,
+        LUXURY,
+        MASTER,
+        NEST,
+        NET,
+        POKE,
+        PREMIER,
+        QUICK,
+        SAFARI,
+        SPORT,
+        TIMER,
+        ULTRA
+    }
+
+    public PokeballType pokeballType = PokeballType.POKE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<Animator>().SetFloat("BallNumber", (int)pokeballType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
